0d8bda3854a75f6d33a4ca2115b556e6790b8cf2
IiIymik <sambotemp@gmail.com>
2021-09-16 19:07:08 +0300

    add file

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
-const user = "vlad";
-const createUsers = function (name) {
+var User = "vlad";
+var createUsers = function (name) {
     console.log(name);
 };
-createUsers(user);
+createUsers(User);
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+var mango = { id: 24 };
+console.log(mango);
--- hello.ts
+++ hello.ts
@@ -1,8 +1,22 @@
-const user: String = "vlad";
+const User: String = "vlad";
 
 const createUsers = <U> (name: U):void => {
     console.log(name)
 }
 
 
-createUsers(user);
+createUsers(User);
+
+const arr: number[] = [1, 2, 3, 4, 5];
+
+
+console.log(arr);
+
+interface User<T> {
+    id: T;
+}
+
+const mango: User<number> = { id: 24 };
+
+console.log(mango);
+
